6169b98cc6820a1475886b1e7bc849b9f2e9b530
Yerson Reyes <yerson.programador@gmail.com>
2020-05-14 18:33:00 -0400

    dependencias instaladas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const openToast = () => toast("Wow so easy !");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={openToast}>click</button>
+      <ToastContainer />
     </div>
   );
 }
