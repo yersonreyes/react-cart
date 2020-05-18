cbeb626acce15d3e1e4542cf60b86a858caf3635
Yerson Reyes <yerson.programador@gmail.com>
2020-05-18 18:57:50 -0400

    Agregando funcion fetch y componente products

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { ToastContainer, toast } from "react-toastify";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect } from "react";
+import TopMenu from "./components/TopMenu";
+import Products from "./components/Products";
+import useFetch from "./hooks/useFetch";
+import { urlApiProducts } from "./utils/constans";
 
-function App() {
-  const openToast = () => toast("Wow so easy !");
+export default function App() {
+  const products = useFetch(urlApiProducts, null);
+
   return (
-    <div className="App">
-      <button onClick={openToast}>click</button>
-      <ToastContainer />
+    <div>
+      <TopMenu />
+      <Products products={products} />
     </div>
   );
 }
-
-export default App;
