840ced43320a36f5ee94a8cef052aa01eaf82e52
Yerson Reyes <yerson.programador@gmail.com>
2020-05-20 01:53:50 -0400

    Funcion para abrir el carrito

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { ReactComponent as CartEmpy } from "../../assets/svg/cart-empty.svg";
 
 import "./Cart.scss";
 
 export default function Cart() {
+  const [cartOpen, setCartOpen] = useState(false);
+  const widthCartContent = cartOpen ? 400 : 0;
+
+  const openCart = () => {
+    setCartOpen(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const closeCart = () => {
+    setCartOpen(false);
+    document.body.style.overflow = "scroll";
+  };
   return (
     <>
       <Button variant="link" className="cart">
-        <CartEmpy />
+        <CartEmpy onClick={openCart} />
       </Button>
-      <div className="cart-content">todos mis productos</div>
+      <div className="cart-content" style={{ width: widthCartContent }}>
+        todos mis productos
+      </div>
     </>
   );
 }
